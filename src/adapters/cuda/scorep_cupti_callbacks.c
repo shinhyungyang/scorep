12462e8287de46533060d90e9a5f9abc040b9709
Christian Rössel <c.roessel@fz-juelich.de>
2013-02-25 11:53:20 +0000

    Reverted revision:3655, i.e. merge of revision:3549 as revision:3538 will not be part of 1.1.1. This fixes a build error in the cuda adapter.
    Subversion: revision:3931

--- src/adapters/cuda/scorep_cupti_callbacks.c
+++ src/adapters/cuda/scorep_cupti_callbacks.c
@@ -388,8 +388,9 @@ scorep_cupti_callback_cuda_runtime( CUpti_CallbackId          callbackId,
     }
     else
     {
+        /* replace SCOREP_REGION_FUNCTION with "CUDART_API" */
         region_handle = SCOREP_DefineRegion( callbackInfo->functionName, NULL, cuda_runtime_file_handle,
-                                             0, 0, SCOREP_ADAPTER_CUDA, SCOREP_REGION_WRAPPER );
+                                             0, 0, SCOREP_ADAPTER_CUDA, SCOREP_REGION_FUNCTION );
 
         cuda_api_function_put( CUPTI_CB_DOMAIN_RUNTIME_API, callbackId, region_handle );
     }
@@ -438,8 +439,9 @@ scorep_cupti_callback_driver_api( CUpti_CallbackId          callbackId,
     }
     else
     {
+        /* replace SCOREP_REGION_FUNCTION with "CUDRV_API" */
         region_handle = SCOREP_DefineRegion( callbackInfo->functionName, NULL, cuda_driver_file_handle,
-                                             0, 0, SCOREP_ADAPTER_CUDA, SCOREP_REGION_WRAPPER );
+                                             0, 0, SCOREP_ADAPTER_CUDA, SCOREP_REGION_FUNCTION );
 
         cuda_api_function_put( CUPTI_CB_DOMAIN_DRIVER_API, callbackId, region_handle );
     }
