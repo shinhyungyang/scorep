836c09ef4bb6f159ab86e4e5257cafe116d64bf8
Bert Wesarg <bert.wesarg@tu-dresden.de>
2023-08-11 14:26:44 +0200

    POSIX I/O: Add config variable

---
+++ src/adapters/io/posix/scorep_posix_io_confvars.inc.c
@@ -0,0 +1,26 @@
+/*
+ * This file is part of the Score-P software (http://www.score-p.org)
+ *
+ * Copyright (c) 2025,
+ * Technische Universitaet Dresden, Germany
+ *
+ * This software may be modified and distributed under the terms of
+ * a BSD-style license. See the COPYING file in the package base
+ * directory for details.
+ *
+ */
+
+
+/**
+ * @file
+ *
+ *
+ */
+
+
+/*
+ *  Configuration variables for the POSIX I/O adapter.
+ */
+static const SCOREP_ConfigVariable scorep_posix_io_confvars[] = {
+    SCOREP_CONFIG_TERMINATOR
+};
