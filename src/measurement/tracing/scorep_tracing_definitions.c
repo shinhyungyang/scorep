1f7ba7f1de37318119c68102700ffabfebe4dc9d
Jan André Reuter <j.reuter@fz-juelich.de>
2023-09-06 12:37:39 +0200

    OMPT: Add OpenMP target types and definitions
    As we want to separate both OpenMP and OpenMP target,
    add all the required paradigms and groups for it.
    
    Signed-off-by: Jan André Reuter <j.reuter@fz-juelich.de>

--- src/measurement/tracing/scorep_tracing_definitions.c
+++ src/measurement/tracing/scorep_tracing_definitions.c
@@ -13,7 +13,7 @@
  * Copyright (c) 2009-2013,
  * University of Oregon, Eugene, USA
  *
- * Copyright (c) 2009-2017, 2022, 2024,
+ * Copyright (c) 2009-2017, 2022-2024,
  * Forschungszentrum Juelich GmbH, Germany
  *
  * Copyright (c) 2009-2013,
