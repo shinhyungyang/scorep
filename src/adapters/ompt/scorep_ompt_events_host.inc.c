tudasc/scorep-xray

[OMPT] Fix stored_task for parallel in tasks
Right now, the stored_task is always updated to the last
encountering_task when a parallel_region is ended. However, this causes
issues when this encountering task is a `#pragma omp task` which is
completed afterwards. When we then try to check if overdue events need
to get triggered, it fails because the pointer is inaccurate.

This change will update the stored_task whenever a task is switched,
fixing the issues showing up for parallel regions inside of tasks.

Signed-off-by: Jan André Reuter <j.reuter@fz-juelich.de>

src/adapters/ompt/scorep_ompt_events_host.inc.c
@@ -2207,6 +2207,21 @@ task_schedule_handle_next_task( task_t*      priorTask,
                          next_task, adapter_tid );
         }
     }
+
+    /* A task may have executed a parallel region. During
+     * scorep_ompt_cb_parallel_end, we then set stored_data->task to
+     * the encountering_task i.e. the task itself. When we now switch to a
+     * different task and trigger overdue events, no event will be triggered,
+     * since the pointers do not match. To fix this, update stored_data->task
+     * on switching tasks. */
+    if ( nextTaskData->ptr != priorTask )
+    {
+        SCOREP_Location* location = SCOREP_Location_GetCurrentCPULocation();
+        GET_SUBSYSTEM_DATA( location, stored_data, unused );
+        UTILS_MutexLock( &stored_data->protect_task_exchange );
+        stored_data->task = nextTaskData->ptr;
+        UTILS_MutexUnlock( &stored_data->protect_task_exchange );
+    }
 }
 
 

shinhyungyang/scorep

src/adapters/ompt/scorep_ompt_events_host.inc.c
@@ -2117,6 +2117,21 @@ scorep_ompt_cb_host_task_schedule( ompt_data_t*       prior_task_data,
         }
     }
 
+    /* A task may have executed a parallel region. During
+     * scorep_ompt_cb_parallel_end, we then set stored_data->task to
+     * the encountering_task i.e. the task itself. When we now switch to a
+     * different task and trigger overdue events, no event will be triggered,
+     * since the pointers do not match. To fix this, update stored_data->task
+     * on switching tasks. */
+    if ( next_task_data->ptr != prior_task )
+    {
+        SCOREP_Location* location = SCOREP_Location_GetCurrentCPULocation();
+        GET_SUBSYSTEM_DATA( location, stored_data, unused );
+        UTILS_MutexLock( &stored_data->protect_task_exchange );
+        stored_data->task = next_task_data->ptr;
+        UTILS_MutexUnlock( &stored_data->protect_task_exchange );
+    }
+
     UTILS_DEBUG_EXIT();
     SCOREP_IN_MEASUREMENT_DECREMENT();
 }