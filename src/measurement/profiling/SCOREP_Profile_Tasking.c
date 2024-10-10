5c038dcad0ecef726b313a2d950b2aac092a9be2
Jan André Reuter <j.reuter@fz-juelich.de>
2024-10-10 11:05:39 +0200

    Profile: Recognize tied tasks correctly
    While the profiling is theoretically able to reuse memory for tied tasks,
    this functionality was never used, as tasks were always recognized as
    tasks that can migrate due to the check happening too late.
    In reality, this should only happen for untied
    tasks.
    
    This fixes Score-P aborting because of excessive memory usage
    of the profiling since each new task were allocating new memory.
    This fix is only targeting tied tasks. Untied tasks keep the same old
    behavior.
    
    Signed-off-by: Jan André Reuter <j.reuter@fz-juelich.de>

--- src/measurement/profiling/SCOREP_Profile_Tasking.c
+++ src/measurement/profiling/SCOREP_Profile_Tasking.c
@@ -13,7 +13,7 @@
  * Copyright (c) 2009-2013,
  * University of Oregon, Eugene, USA
  *
- * Copyright (c) 2009-2013, 2015, 2017, 2022,
+ * Copyright (c) 2009-2013, 2015, 2017, 2022, 2024,
  * Forschungszentrum Juelich GmbH, Germany
  *
  * Copyright (c) 2009-2015,
@@ -210,6 +210,10 @@ SCOREP_Profile_TaskBegin( SCOREP_Location*                 thread,
     scorep_profile_type_set_region_handle( &specific_data, regionHandle );
 
     scorep_profile_task* task = get_profile_task_data( taskHandle );
+    /* Before allocating any potential memory, we must set if the task can migrate, as this can
+     * have an impact on the way memory is allocated. */
+    task->can_migrate
+        = ( SCOREP_RegionHandle_GetType( regionHandle ) == SCOREP_REGION_TASK_UNTIED );
 
     scorep_profile_node* task_root
         = create_task_root( location, regionHandle, timestamp, metricValues,
@@ -218,8 +222,6 @@ SCOREP_Profile_TaskBegin( SCOREP_Location*                 thread,
     task->current_node = task_root;
     task->root_node    = task_root;
     task->depth        = 1;
-    task->can_migrate
-        = ( SCOREP_RegionHandle_GetType( regionHandle ) == SCOREP_REGION_TASK_UNTIED );
 
     /* Perform activation */
     scorep_profile_task_switch_start( location, task, timestamp, metricValues );
