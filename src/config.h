9bb7bee6d09fa7e4f496ec75299bd161a6f1f98d
Gregor Corbin <g.corbin@fz-juelich.de>
2025-01-27 13:33:21 +0100

    MPIF08, config.h: remove indentation
    This is necessary to include the file from Fortran:
    The Fortran preprocessor cannot handle indented directives.

--- src/config.h
+++ src/config.h
@@ -13,7 +13,7 @@
  * Copyright (c) 2009-2011,
  * University of Oregon, Eugene, USA
  *
- * Copyright (c) 2009-2011, 2023,
+ * Copyright (c) 2009-2011, 2023, 2025,
  * Forschungszentrum Juelich GmbH, Germany
  *
  * Copyright (c) 2009-2011,
@@ -25,7 +25,14 @@
  * This software may be modified and distributed under the terms of
  * a BSD-style license. See the COPYING file in the package base
  * directory for details.
+ */
+
+/* This file is also included from Fortran. The Fortran preprocessor does not
+ * support all features of the C preprocessor, in particular:
+ * - preprocessor directives have to start at column 1,
+ * - the HAVE_ macros defined here cannot be used in Fortran.
  *
+ * Note: C-comments are stripped by both preprocessors.
  */
 
 #ifndef CONFIG_H
@@ -40,50 +47,50 @@
  */
 
 #if defined CROSS_BUILD
-    #if defined FRONTEND_BUILD
-        #include <config-frontend.h>
-        #include <config-backend-for-frontend.h>
-        #define HAVE_BACKEND( H ) ( defined( HAVE_BACKEND_ ## H ) && HAVE_BACKEND_ ## H )
-    #elif defined BACKEND_BUILD_NOMPI
-        #include <config-backend.h>
-        #define HAVE_BACKEND( H ) ( defined( HAVE_ ## H ) && HAVE_ ## H )
-    #elif defined BACKEND_BUILD_MPI
-        #include <config-backend-mpi.h>
-        #define HAVE_BACKEND( H ) ( defined( HAVE_ ## H ) && HAVE_ ## H )
-    #elif defined BACKEND_BUILD_SHMEM
-        #include <config-backend-shmem.h>
-        #define HAVE_BACKEND( H ) ( defined( HAVE_ ## H ) && HAVE_ ## H )
-    #else
-        #error "You cannot use config.h without defining either FRONTEND_BUILD, BACKEND_BUILD_NOMPI, BACKEND_BUILD_MPI or BACKEND_BUILD_SHMEM."
-    #endif
+#if defined FRONTEND_BUILD
+#include <config-frontend.h>
+#include <config-backend-for-frontend.h>
+#define HAVE_BACKEND( H ) ( defined( HAVE_BACKEND_ ## H ) && HAVE_BACKEND_ ## H )
+#elif defined BACKEND_BUILD_NOMPI
+#include <config-backend.h>
+#define HAVE_BACKEND( H ) ( defined( HAVE_ ## H ) && HAVE_ ## H )
+#elif defined BACKEND_BUILD_MPI
+#include <config-backend-mpi.h>
+#define HAVE_BACKEND( H ) ( defined( HAVE_ ## H ) && HAVE_ ## H )
+#elif defined BACKEND_BUILD_SHMEM
+#include <config-backend-shmem.h>
+#define HAVE_BACKEND( H ) ( defined( HAVE_ ## H ) && HAVE_ ## H )
+#else
+#error "You cannot use config.h without defining either FRONTEND_BUILD, BACKEND_BUILD_NOMPI, BACKEND_BUILD_MPI or BACKEND_BUILD_SHMEM."
+#endif
 
 #elif defined NOCROSS_BUILD
-    #if defined BACKEND_BUILD_NOMPI
-        #include <config-backend.h>
-    #elif defined BACKEND_BUILD_MPI
-        #include <config-backend-mpi.h>
-    #elif defined BACKEND_BUILD_SHMEM
-        #include <config-backend-shmem.h>
-    #else
-        #error "You cannot use config.h without defining either BACKEND_BUILD_NOMPI, BACKEND_BUILD_MPI or BACKEND_BUILD_SHMEM."
-    #endif
+#if defined BACKEND_BUILD_NOMPI
+#include <config-backend.h>
+#elif defined BACKEND_BUILD_MPI
+#include <config-backend-mpi.h>
+#elif defined BACKEND_BUILD_SHMEM
+#include <config-backend-shmem.h>
+#else
+#error "You cannot use config.h without defining either BACKEND_BUILD_NOMPI, BACKEND_BUILD_MPI or BACKEND_BUILD_SHMEM."
+#endif
 
-    #define HAVE_BACKEND( H ) ( defined( HAVE_ ## H ) && HAVE_ ## H )
+#define HAVE_BACKEND( H ) ( defined( HAVE_ ## H ) && HAVE_ ## H )
 
 #elif defined SCORE_BUILD
-    #include <config-score.h>
+#include <config-score.h>
 
 #elif defined GCC_PLUGIN_BUILD
-       #include <config-gcc-plugin.h>
+#include <config-gcc-plugin.h>
 
 #elif defined LLVM_PLUGIN_BUILD
        #include <config-llvm-plugin.h>
 
 #elif defined LIBWRAP_BUILD
-    #include <config-libwrap.h>
+#include <config-libwrap.h>
 
 #else
-    #error "You cannot use config.h without defining either CROSS_BUILD or NOCROSS_BUILD."
+#error "You cannot use config.h without defining either CROSS_BUILD or NOCROSS_BUILD."
 #endif
 
 #include <config-common.h>
