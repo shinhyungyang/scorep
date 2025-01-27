3307bbeac5379a79763c475f472316ca9ed2eaff
Gregor Corbin <g.corbin@fz-juelich.de>
2025-01-27 13:33:23 +0100

    MPIF08, adapter: factor out topo_create_cart_definition

---
+++ src/adapters/mpi/include/scorep_mpi_topo.h
@@ -0,0 +1,50 @@
+/*
+ * This file is part of the Score-P software (http://www.score-p.org)
+ *
+ * Copyright (c) 2009-2013,
+ * RWTH Aachen University, Germany
+ *
+ * Copyright (c) 2009-2013,
+ * Gesellschaft fuer numerische Simulation mbH Braunschweig, Germany
+ *
+ * Copyright (c) 2009-2017,
+ * Technische Universitaet Dresden, Germany
+ *
+ * Copyright (c) 2009-2013,
+ * University of Oregon, Eugene, USA
+ *
+ * Copyright (c) 2009-2016, 2022-2023, 2025,
+ * Forschungszentrum Juelich GmbH, Germany
+ *
+ * Copyright (c) 2009-2014,
+ * German Research School for Simulation Sciences GmbH, Juelich/Aachen, Germany
+ *
+ * Copyright (c) 2009-2013,
+ * Technische Universitaet Muenchen, Germany
+ *
+ * Copyright (c) 2016,
+ * Technische Universitaet Darmstadt, Germany
+ *
+ * This software may be modified and distributed under the terms of
+ * a BSD-style license. See the COPYING file in the package base
+ * directory for details.
+ *
+ */
+
+/**
+ * @file
+ * @ingroup    MPI_Wrapper
+ *
+ * @brief Topologies
+ */
+
+#ifndef SCOREP_MPI_TOPO_H
+#define SCOREP_MPI_TOPO_H
+
+#include <mpi.h>
+
+void
+scorep_mpi_topo_create_cart_definition( char*     namePrefix,
+                                        MPI_Comm* cartComm );
+
+#endif /* SCOREP_MPI_TOPO_H */
