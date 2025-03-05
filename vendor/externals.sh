d683e2d05c8735ddcf18fa48c23c564ecfb417f8
Christian Feld <c.feld@fz-juelich.de>
2025-03-05 09:32:49 +0100

    externals: Update to releases

--- vendor/externals.sh
+++ vendor/externals.sh
@@ -46,7 +46,7 @@ cmd_help()
 
 cmd=cmd_${1:-get}
 
-${cmd} otf2 -b v3.1-rc5 https://gitlab.jsc.fz-juelich.de/perftools/otf2.git
+${cmd} otf2 -b v3.1.1 https://gitlab.jsc.fz-juelich.de/perftools/otf2.git
 ${cmd} opari2 -b v2.0.9 https://gitlab.jsc.fz-juelich.de/perftools/opari2.git
-${cmd} cubew -b v4.9-rc3 https://gitlab.jsc.fz-juelich.de/perftools/cubew.git
-${cmd} cubelib -b v4.9-rc3 https://gitlab.jsc.fz-juelich.de/perftools/cubelib.git
+${cmd} cubew -b v4.9 https://gitlab.jsc.fz-juelich.de/perftools/cubew.git
+${cmd} cubelib -b v4.9 https://gitlab.jsc.fz-juelich.de/perftools/cubelib.git
