shinhyungyang/scorep

commit dc242f071684f9fa0cb7473d31219a46b3169c1f

tracing: Honor all definitions when determining the chunk size
Using only the #locations might be too low if other definitions exceed
the requirements.

Closes #396

src/measurement/tracing/SCOREP_Tracing.c
@@ -7,7 +7,7 @@
  * Copyright (c) 2009-2013,
  * Gesellschaft fuer numerische Simulation mbH Braunschweig, Germany
  *
- * Copyright (c) 2009-2016, 2019-2020,
+ * Copyright (c) 2009-2016, 2019-2020, 2025,
  * Technische Universitaet Dresden, Germany
  *
  * Copyright (c) 2009-2013,
@@ -55,6 +55,7 @@
 #include "scorep_tracing_definitions.h"
 #include <scorep_clock_synchronization.h>
 #include "scorep_tracing_internal.h"
+#include <scorep_ipc.h>
 
 #include <SCOREP_Substrates_Management.h>
 #include <scorep_substrates_definition.h>
@@ -477,14 +478,46 @@ write_definitions( void )
 
     OTF2_ErrorCode ret;
 
+    /* Local definition ID mappings needs to be considered here too. The estimator
+     * does not provide this directly. But ID maps are written either as a dense
+     * or sparse array, whatever is smaller. So taking the dense array as the
+     * upper bound should be ok. The estimator only honors #locations, #regions,
+     * and #metrics. Hence we determine a local maximum out of all local definitions
+     * for which we write ID mappings and use this as the #locations. Use @locations
+     * as it is uint64_t. */
+    uint64_t local_max_definition_count = scorep_tracing_get_max_local_definition_count();
+    uint64_t max_definition_count       = 0;
+    SCOREP_Ipc_Reduce( &local_max_definition_count,
+                       &max_definition_count,
+                       1,
+                       SCOREP_IPC_UINT64_T,
+                       SCOREP_IPC_MAX,
+                       0 );
     uint64_t definition_chunk_size = OTF2_UNDEFINED_UINT64;
     if ( SCOREP_Status_GetRank() == 0 )
     {
+        /* The estimator takes the maximum of the Location, Region, and Metric
+         * definitions. For Locations it always takes the 9 bytes, but since
+         * they do not have to be consecutive, they create a potentially tight
+         * estimate. Both Metric and Region must be consecutive, so we combine
+         * the maximum number of local definitions with the unified Region
+         * definition count and keep the number of Location and Metric
+         * definitions to the unified count. */
         OTF2_EventSizeEstimator* estimator = OTF2_EventSizeEstimator_New();
-        /* We do not write metric or region groups, thus we set only the number of locations */
+        UTILS_ASSERT( estimator );
         OTF2_EventSizeEstimator_SetNumberOfLocationDefinitions(
             estimator,
             scorep_unified_definition_manager->location.counter );
+        if ( scorep_unified_definition_manager->region.counter > max_definition_count )
+        {
+            max_definition_count = scorep_unified_definition_manager->region.counter;
+        }
+        OTF2_EventSizeEstimator_SetNumberOfRegionDefinitions(
+            estimator,
+            max_definition_count );
+        OTF2_EventSizeEstimator_SetNumberOfMetricDefinitions(
+            estimator,
+            scorep_unified_definition_manager->metric.counter );
         definition_chunk_size = OTF2_EventSizeEstimator_GetDefChunkSize( estimator );
         OTF2_EventSizeEstimator_Delete( estimator );
     }

src/measurement/tracing/scorep_tracing_definitions.c
@@ -7,7 +7,7 @@
  * Copyright (c) 2009-2013,
  * Gesellschaft fuer numerische Simulation mbH Braunschweig, Germany
  *
- * Copyright (c) 2009-2019, 2022, 2024,
+ * Copyright (c) 2009-2019, 2022, 2024-2025,
  * Technische Universitaet Dresden, Germany
  *
  * Copyright (c) 2009-2013,
@@ -1525,6 +1525,39 @@ scorep_write_io_handle_definitions( void*                     writerHandle,
         }
     }
     SCOREP_DEFINITIONS_MANAGER_FOREACH_DEFINITION_END();
+}
+
+
+uint64_t
+scorep_tracing_get_max_local_definition_count( void )
+{
+    uint64_t max_definition_count = 0;
+
+    #define MAX_DEFINITION_COUNT( type ) \
+    if ( scorep_local_definition_manager.type.counter > max_definition_count ) \
+    { \
+        max_definition_count = scorep_local_definition_manager.type.counter; \
+    }
+
+    MAX_DEFINITION_COUNT( string )
+    MAX_DEFINITION_COUNT( location )
+    MAX_DEFINITION_COUNT( location_group )
+    MAX_DEFINITION_COUNT( region )
+    MAX_DEFINITION_COUNT( group )
+    MAX_DEFINITION_COUNT( interim_communicator )
+    MAX_DEFINITION_COUNT( rma_window )
+    MAX_DEFINITION_COUNT( sampling_set )
+    MAX_DEFINITION_COUNT( attribute )
+    MAX_DEFINITION_COUNT( source_code_location )
+    MAX_DEFINITION_COUNT( calling_context )
+    MAX_DEFINITION_COUNT( interrupt_generator )
+    MAX_DEFINITION_COUNT( io_file )
+    MAX_DEFINITION_COUNT( io_handle )
+    MAX_DEFINITION_COUNT( parameter )
+
+    #undef MAX_DEFINITION_COUNT
+
+    return max_definition_count;
 }
 
 

src/measurement/tracing/scorep_tracing_definitions.h
@@ -7,7 +7,7 @@
  * Copyright (c) 2009-2011,
  * Gesellschaft fuer numerische Simulation mbH Braunschweig, Germany
  *
- * Copyright (c) 2009-2011,
+ * Copyright (c) 2009-2011, 2025,
  * Technische Universitaet Dresden, Germany
  *
  * Copyright (c) 2009-2011,
@@ -54,6 +54,12 @@ scorep_tracing_write_global_definitions( OTF2_GlobalDefWriter* global_definition
 
 void
 scorep_tracing_set_properties( OTF2_Archive* scorep_otf2_archive );
+
+/**
+ * Returns the upper bound for all local definitions tracing writes out mappings.
+ */
+uint64_t
+scorep_tracing_get_mapped_definitions_upper_bound( void );
 
 
 #endif /* SCOREP_TRACING_DEFINITIONS_H */

commit 8e6ff98e57547096e49f3e1e2fae45f45093e02a

tracing: Fix broken build
Broken by dc242f071684f9fa0cb7473d31219a46b3169c1f

src/measurement/tracing/SCOREP_Tracing.c
@@ -485,7 +485,7 @@ write_definitions( void )
      * and #metrics. Hence we determine a local maximum out of all local definitions
      * for which we write ID mappings and use this as the #locations. Use @locations
      * as it is uint64_t. */
-    uint64_t local_max_definition_count = scorep_tracing_get_max_local_definition_count();
+    uint64_t local_max_definition_count = scorep_tracing_get_mapped_definitions_upper_bound();
     uint64_t max_definition_count       = 0;
     SCOREP_Ipc_Reduce( &local_max_definition_count,
                        &max_definition_count,

src/measurement/tracing/scorep_tracing_definitions.c
@@ -1529,7 +1529,7 @@ scorep_write_io_handle_definitions( void*                     writerHandle,
 
 
 uint64_t
-scorep_tracing_get_max_local_definition_count( void )
+scorep_tracing_get_mapped_definitions_upper_bound( void )
 {
     uint64_t max_definition_count = 0;
 